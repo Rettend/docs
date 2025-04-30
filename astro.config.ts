3f1c8cc4237431d2e2ca563f327a650625ef4bbb
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-04-30 12:01:01 +0200

    init

---
+++ astro.config.ts
@@ -0,0 +1,24 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/rettend/docs' }],
+      customCss: ['@fontsource/inter/400.css', '@fontsource/inter/600.css', './src/styles/custom.css'],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+})
